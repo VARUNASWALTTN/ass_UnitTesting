e00bbc9400f8e1f443abcccb579e7b07ee59a247
VARUNASWALTTN <114069188+VARUNASWALTTN@users.noreply.github.com>
2022-10-09 00:26:37 +0530

    Add files via upload

---
+++ unit-testing-master/src/test/java/com/demo/service/EmailServiceTest.java
@@ -0,0 +1,14 @@
+package com.demo.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+
+    @Test
+    public void
+
+
+}
